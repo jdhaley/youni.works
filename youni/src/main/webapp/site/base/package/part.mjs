caa84c9004df61cb5df9050cc939ea1f73fd6f46
jdhal <jdhaley@gmail.com>
2020-05-21 22:06:02 -0400

    base - now consists of signal & part...
    Major refactoring to refine the interfaces.

---
+++ youni/src/main/webapp/site/base/package/part.mjs
@@ -0,0 +1,64 @@
+let LAST_ID = 0;
+
+export default {
+	package$: "youni.works/part",
+	package$signal: "youni.works/signal",
+	Part: {
+		super$: "Object",
+		once$id: () => ++LAST_ID,
+		type$of: "Part",
+		part: {
+		},
+		once$owner: function() {
+			//owner is optional.
+			return this.of && this.of != this && this.of.owner;
+		},
+		once$name: function() {
+			if (this.of) for (let name in this.of.part) {
+				if (this.part[name] == this) return name;
+			}
+			return "";
+		},
+		get$pathname: function() {
+			if (this.name) {
+				let path = this.of && this.of.pathname || "";
+				return path + "/" + this.name;
+			}
+			return "";
+		},
+		get$log: function() {
+			return this.owner ? this.owner.log : console;
+		},
+		initialize: function(conf) {
+			if (this.of != this.sys.interfaceOf(this, "Part")) {
+				throw new Error("Component is already initialized.");
+			}
+			this.sys.define(this, "of", conf.component, "const");
+			conf.component = this;
+		}
+	},
+	Component: {
+		super$: "Part",
+		//TODO super$: "Part Controller" - true implement
+		type$receive: "signal.Controller.receive",
+		type$control: "signal.Controller.control",
+		type$process: "signal.Controller.process",
+		type$action: "signal.Controller.action"
+	},
+	Service: {
+		super$: "Object",
+		service: function(receiver, subject, request) {
+			let message = this.createMessage(receiver, subject, request);
+			message.status = 204;
+			on.receive(message);
+		},
+		createMessage(receiver, subject, request) {
+			let message = this.sys.extend();
+			message[Symbol.Signal] = "Message";
+			message.action = subject;
+			message.request = request;
+			message.status = 0;
+			return message;
+		}
+	}
+}
---
+++ youni/src/main/webapp/site/base/package/signal.mjs
@@ -0,0 +1,72 @@
+Symbol.Signal = Symbol("signal");
+
+export default {
+	package$: "youni.works/signal",
+	Message: {
+		super$: "Object",
+		var$action: ""
+	},
+	Receiver: {
+		super$: "Object",
+		receive: function(message) {
+			if (message[Symbol.Signal] == "Call") {
+				let action = this[message.action];
+				if (typeof action == "function") {
+					return message.apply
+						? action.apply(this, message)
+						: action.call(this, message);
+
+				}
+			} else {
+				this.controller && this.controller.process(this, message);
+			}
+		}
+	},
+	Sender: {
+		super$: "Receiver",
+		after$receive: function(message) {
+			this.send(this, message);
+			return Function.RETURNED;
+		},
+		send: function send(to, message) {
+			let signal = message[Symbol.Signal] || "Call";
+			this.signal[signal].call(this, to, message);
+		},
+		extend$signal: {
+			Call: function signal(on, message) {
+				for (let name in on.part) {
+					if (!message.action) return;
+					let part = on.part[name];
+					part.receive && part.receive(message);
+					signal(part, message);
+				}
+			}
+		}
+	},
+	Controller: {
+		super$: "Receiver",
+		control: function(control) {
+			control.controller = this;
+		},
+		process: function(on, message) {
+			let action = message.action;
+			while (action && this.action[action]) try {
+				this.action[action].call(this, on, message);
+				action = (message.action == action ? "" : message.action);
+			} catch (error) {
+				error.message = `Error processing action "${message.action}": ` + error.message;
+				/* Stop the message if the exception action threw an error */
+				if (message.action == "exception") throw error;
+				message.error = error;
+				message.action = "exception";
+				return this.process(on, message);
+			}
+		},
+		action: {
+			exception: function(on, message) {
+				this.log.error(message.error);
+				message.action = "";
+			}
+		}
+	}
+}
