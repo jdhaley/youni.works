deb1958a5a6bfaf2c71eabff454bd5d520825100
jdhal <jdhaley@gmail.com>
2020-05-22 15:04:38 -0400

    loader: focus on serializing JSON...
    own test.mjs based off system.mjs

--- youni/src/main/webapp/site/loader/conf.mjs
+++ youni/src/main/webapp/site/loader/conf.mjs
@@ -1,12 +1,12 @@
 import signal		from "../base/package/signal.mjs";
 import part			from "../base/package/part.mjs";
 import member		from "./package/member.mjs";
-
+import test			from "./test.mjs";
 export default {
 	packages: {
 		signal: signal,
 		part: part,
 		member: member,
 	},
-	test: member,
+	test: test
 }
