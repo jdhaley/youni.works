e8cffd2e97ea54ba77fb0b755eb333098ac39c3a
jdhal <jdhaley@gmail.com>
2020-05-29 20:00:15 -0400

    clean up server.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
-const fs = require('fs');
-const https = require('https');
-
 const express = require("express");
-
-const credentials = {
-	key: fs.readFileSync('ssl/my.key', 'utf8'),
-	cert: fs.readFileSync('ssl/my.crt', 'utf8')
-};
 const app = express();
-const httpsServer = https.createServer(credentials, app);
-const port = 443;
-
-httpsServer.listen(port);
 
 app.use("/prd", express.static("site"));
 app.use("/file", express.static("../fs"));
 
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const fs = require("fs");
+const credentials = {
+	key: fs.readFileSync("ssl/my.key", "utf8"),
+	cert: fs.readFileSync("ssl/my.crt", "utf8")
+};
+const https = require("https");
+const httpsServer = https.createServer(credentials, app);
+const port = 443;
+
+httpsServer.listen(port, () => console.log(`HTTPS Server listening on port "${port}"`));
