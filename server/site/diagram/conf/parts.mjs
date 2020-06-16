79069c371f44d2748c03254176660c7923799b0a
jdhal <jdhaley@gmail.com>
2020-06-16 05:52:29 -0400

    diagram: draws rectangles as discussed...
    via Ctrl+Click for now.

--- server/site/diagram/conf/parts.mjs
+++ server/site/diagram/conf/parts.mjs
@@ -20,6 +20,42 @@ export default {
 						},
 						article: {
 							type$: "use.graphic.Graphic",
+							cellSize: 20,
+							part: {
+								rect: {
+									type$: "use.graphic.Shaper",
+									viewName: "rect",
+									width: 4,
+									height: 2,
+									fill: "none",
+									stroke: "green",
+									draw: function(g, x, y) {
+										let cell = g.cellSize;
+										let rect = this.view();
+										x -= x % cell;
+										y -= y % cell;
+										rect.setAttribute("x", x - this.width * cell / 2);
+										rect.setAttribute("y", y - this.height * cell / 2);
+										rect.setAttribute("width", this.width * cell);
+										rect.setAttribute("height", this.height * cell);
+										rect.setAttribute("fill", this.fill);
+										rect.setAttribute("stroke", this.stroke);
+										return rect;										
+									}
+								},
+								ellipse: {
+									type$: "use.graphic.Shaper",
+									viewName: "ellipse"									
+								}
+							},
+							action: {
+								Click: function(on, event) {					
+									if (event.ctrlKey) {
+										let rect = this.part.rect.draw(this, event.offsetX, event.offsetY);
+										on.append(rect);
+									}
+								}
+							}
 						}
 					},
 					extend$shortcut: {
