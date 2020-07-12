3e26d78d25fd21dc51063abaa8697903c039e2a0
jdhal <jdhaley@gmail.com>
2020-07-11 21:23:53 -0400

    model.Node supports .text & .markup

--- server/site/compiler/package/model.mjs
+++ server/site/compiler/package/model.mjs
@@ -23,14 +23,18 @@ export default {
 	Node: {
 		super$: "Object",
 		type$content: "Content",
-		get$textContent: function() {
+		get$text: function() {
 			if (typeof this.content == "string") return this.content;
-			let textContent = "";
-			for (let value of this.content) {
-				let text = value && value.textContent;
-				if (text) textContent += text;
-			}
-			return textContent;
+			let text = "";
+			for (let value of this.content) text += textOf(value);
+			return text;
+		},
+		get$markup: function() {
+			let name = this.name ? markupString(this.name) : "";
+			let markup = name ? "<" + name + ">" : "";
+			for (let value of this.content) markup += markupOf(value);
+			if (name) markup += "</" + name + ">";
+			return markup;
 		},
 		accept: function() {
 			if (!arguments.length) return 0;
@@ -43,6 +47,57 @@ export default {
 	}
 }
 
+function textOf(value) {
+	switch (typeof value) {
+		case "boolean":
+			return "" + value;
+		case "number":
+			return isNaN(value) ? "" : "" + value;
+		case "string":
+			return value;
+		case "object":
+			return value && "" + value.text || "";
+	}
+	return "";
+}
+
+function markupOf(value) {
+	switch (typeof value) {
+		case "boolean":
+			return "" + value;
+		case "number":
+			return isNaN(value) ? "" : "" + value;
+		case "string":
+			return markupString(value);
+		case "object":
+			return value && "" + value.markup || "";
+		}
+	return "";
+}
+
+function markupString(value) {
+	value = "" + value;
+	//NB: Apostrophes are NOT escaped.
+	
+	//Let's assume it is cheaper to check if a new string is required:
+	if (value.indexOf("<") < 0
+		&& value.indexOf(">") < 0
+		&& value.indexOf("&") < 0
+		&& value.indexOf("\"") < 0
+	) return value;			
+
+	let markup = "";
+	for (let i = 0, ch = value.charAt(i), length = value.length; i < length; ch = value.charAt(++i)) {
+		switch (ch) {
+			case "<": markup += "&lt;";		break;
+			case ">": markup += "&gt;";		break;
+			case "&": markup += "&amp;";	break;
+			case "\"": markup += "&quot;";	break;
+			default:  markup += ch;			break;
+		}
+	}
+	return markup;
+}
 //Content: {
 //"@iterator": function* iterate() {
 //	for (let i = 0, len = this.length; i < len; i++) yield this.at(i);
