f30eef5020ec2ce0e051a02abee3a77abef99dd3
jdhal <jdhaley@gmail.com>
2020-07-15 09:46:36 -0400

    removed Rule parser.

--- server/site/compiler/conf/test-match-rules.mjs
+++ server/site/compiler/conf/test-match-rules.mjs
@@ -3,46 +3,43 @@ export default
 	package$: false,
 	package$parser: "youni.works/compiler/parser",
 	main: {
-		type$: "parser.Rule",
-		expr: {
-			type$: "parser.Choice",
-			choice: [
-				{
-					type$: "parser.Production",
-					name: "array",
-					expr: {
-						type$: "parser.Sequence",
-						sequence: [
-							{
-								type$: "parser.Match",
-								suppress: true,
-								nodeName: "pn",
-								nodeText: "[",
-								min: 1,
-								max: 1
-							},
-							{
-								type$: "parser.Match",
-								nodeName: "pn",
-								nodeText: "]",
-								negate: true
-							},
-							{
-								type$: "parser.Match",
-								suppress: true,
-								nodeName: "pn",
-								nodeText: "]",
-								min: 1,
-								max: 1
-							}
-						]
-					}
-				},
-				{
-					type$: "parser.Match",
-					max: 1
+		type$: "parser.Choice",
+		choice: [
+			{
+				type$: "parser.Production",
+				name: "array",
+				expr: {
+					type$: "parser.Sequence",
+					sequence: [
+						{
+							type$: "parser.Match",
+							suppress: true,
+							nodeName: "pn",
+							nodeText: "[",
+							min: 1,
+							max: 1
+						},
+						{
+							type$: "parser.Match",
+							nodeName: "pn",
+							nodeText: "]",
+							negate: true
+						},
+						{
+							type$: "parser.Match",
+							suppress: true,
+							nodeName: "pn",
+							nodeText: "]",
+							min: 1,
+							max: 1
+						}
+					]
 				}
-			]
-		}
-	},
+			},
+			{
+				type$: "parser.Match",
+				max: 1
+			}
+		]
+	}
 }
--- server/site/compiler/conf/test-token-rules.mjs
+++ server/site/compiler/conf/test-token-rules.mjs
@@ -3,114 +3,111 @@ export default
 	package$: false,
 	package$parser: "youni.works/compiler/parser",
 	main: {
-		type$: "parser.Rule",
-		expr: {
-			type$: "parser.Choice",
-			choice: [
-				{
-					type$: "parser.Choice",
-					name: "ws",
-					choice: " \t\r\n"
-				},
-				{
+		type$: "parser.Choice",
+		choice: [
+			{
+				type$: "parser.Choice",
+				name: "ws",
+				choice: " \t\r\n"
+			},
+			{
+				type$: "parser.Sequence",
+				sequence: [
+					{
+						use$: "commentStart",
+					},
+					{
+						type$: "parser.Choice",
+						choice: [
+							{use$: "commentEnd"}
+						],
+						negate: true
+					},
+					{
+						use$: "commentEnd"
+					}
+				]
+			},
+			{
+				type$: "parser.Production",
+				name: "number",	
+				expr: {
 					type$: "parser.Sequence",
 					sequence: [
 						{
-							use$: "commentStart",
+							type$: "parser.Choice",
+							choice: "+-",
+							max: 1
 						},
 						{
 							type$: "parser.Choice",
-							choice: [
-								{use$: "commentEnd"}
-							],
-							negate: true
+							choice: "0123456789",
+							min: 1
+						}
+						//Just do integers for now.
+					]
+				}
+			},
+			{
+				type$: "parser.Production",
+				name: "pn",
+				expr: {
+					type$: "parser.Choice",
+					min: 1,
+					max: 1,
+					choice: "{}()[];,:.@#^*/%+-<=>!&|~?"
+				}
+			},
+			{
+				type$: "parser.Production",
+				name: "word",
+				expr: {
+					"type$": "parser.Sequence",
+					"sequence": [
+						{
+							use$: "letter",
 						},
 						{
-							use$: "commentEnd"
+							"type$": "parser.Choice",
+							"choice": [
+								{
+									use$: "letter"
+								},
+								{
+									use$: "digit"
+								}
+							]
 						}
 					]
-				},
-				{
-					type$: "parser.Production",
-					name: "number",	
-					expr: {
-						type$: "parser.Sequence",
-						sequence: [
-							{
-								type$: "parser.Choice",
-								choice: "+-",
-								max: 1
-							},
-							{
-								type$: "parser.Choice",
-								choice: "0123456789",
-								min: 1
-							}
-							//Just do integers for now.
-						]
-					}
-				},
-				{
-					type$: "parser.Production",
-					name: "pn",
-					expr: {
-						type$: "parser.Choice",
-						min: 1,
-						max: 1,
-						choice: "{}()[];,:.@#^*/%+-<=>!&|~?"
-					}
-				},
-				{
-					type$: "parser.Production",
-					name: "word",
-					expr: {
-						"type$": "parser.Sequence",
-						"sequence": [
-							{
-								use$: "letter",
-							},
-							{
-								"type$": "parser.Choice",
-								"choice": [
-									{
-										use$: "letter"
-									},
-									{
-										use$: "digit"
-									}
-								]
-							}
-						]
-					}
-				},
-				{
-					type$: "parser.Sequence",
-					sequence: [
-						"\"",
-						{
-							type$: "parser.Production",
-							name: "string",
-							expr: {
-								type$: "parser.Choice",
-								choice: [
-									{
-										type$: "parser.Sequence",
-										sequence: "\\\""
-									},
-									{
-										type$: "parser.Choice",
-										choice: "\"",
-										negate: true
-									}
-								]
-							}
-						},
-						"\""
-					],
-					max: 1
 				}
-			]
-		}
+			},
+			{
+				type$: "parser.Sequence",
+				sequence: [
+					"\"",
+					{
+						type$: "parser.Production",
+						name: "string",
+						expr: {
+							type$: "parser.Choice",
+							choice: [
+								{
+									type$: "parser.Sequence",
+									sequence: "\\\""
+								},
+								{
+									type$: "parser.Choice",
+									choice: "\"",
+									negate: true
+								}
+							]
+						}
+					},
+					"\""
+				],
+				max: 1
+			}
+		]
 	},
 	upper: {
 		type$: "parser.Choice",
