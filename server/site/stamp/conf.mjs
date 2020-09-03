8ea2e411842b89a65f773b59e9d4a7617e1cc2e4
jdhal <jdhaley@gmail.com>
2020-09-03 06:14:21 -0400

    sidebar on simple loader

--- server/site/stamp/conf.mjs
+++ server/site/stamp/conf.mjs
@@ -6,12 +6,16 @@ import types from "./conf/types.mjs";
 
 import testModel from "./testModel.mjs";
 
+import loader from "./package/loader.mjs";
+
 export default {
 	packages: {
 		control: control,
 		layout: layout,
-		item: item
+		item: item,
+		loader: loader
 	},
+	testLoader: control,
 	model: testModel,
 	types: types
 }
--- server/site/stamp/main.mjs
+++ server/site/stamp/main.mjs
@@ -1,12 +1,13 @@
 export default function main(sys, conf) {
 	conf.packages = sys.load(conf.packages);
-	document.types = conf.types;
 	const pkg = conf.packages;
+	document.types = conf.types;
 	pkg.layout.Album.createView(document.body, conf.model);
 
 	pkg.item.TableWindow.show(document.body, {
 		type: "Printing"
 	}, conf.model.issues[0].printings);
+	testLoad(sys, conf);
 //	let printing = sys.extend(pkg.control.Record, {
 //		fields: conf.types["Printing"]
 //	});
@@ -15,4 +16,10 @@ export default function main(sys, conf) {
 //	});
 //	let win = pkg.item.Window.createView(document.body);
 //	printings.createView(win.body, conf.model.issues[0].printings);
+}
+
+function testLoad(sys, conf) {
+	
+	let target = conf.packages.loader.Compiler.compile(conf.testLoader, "/test");
+	console.log(target);
 }
