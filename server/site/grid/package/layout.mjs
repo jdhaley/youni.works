82c2974a4f3f291e04c3958ce644c84c8a62dbad
jdhal <jdhaley@gmail.com>
2020-09-26 20:47:07 -0400

    minor

--- server/site/grid/package/layout.mjs
+++ server/site/grid/package/layout.mjs
@@ -76,8 +76,11 @@ export default {
 			});
 		},
 		drawData: function(view) {
-			view.data = this.owner.append(view, "span.data");
-			if (view.shape.image) view.data.style.webkitTextStroke = ".2mm rgba(255, 255, 255, .25)";
+			view.data = this.owner.append(view, ".data");
+			if (view.shape.image) {
+				view.data.style.color = "black";
+				view.data.style.webkitTextStroke = ".1mm rgba(255, 255, 255, .5)";
+			}
 			view.data.innerHTML = this.shapeData(view)
 		},
 		drawPath: function(view) {
