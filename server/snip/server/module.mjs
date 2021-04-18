8c51a3aeca8af64cb2d5e8f0e38f0016f8cee4e6
jdhal <jdhaley@gmail.com>
2021-04-18 04:21:51 -0400

    housekeeping - more stuff moved into snip

---
+++ server/snip/server/module.mjs
@@ -0,0 +1,8 @@
+import system from "../sys/index.mjs";
+
+export default {
+	id: "server.youni.works",
+	version: "1",
+	moduleType: "server",
+	uses: [system]
+}
---
+++ server/snip/server/package/util.mjs
@@ -0,0 +1,79 @@
+export default {
+	Remote: {
+		type$: "system.youni.works/Instance",
+		method: "HEAD",
+		url: "",
+		service: function(receiver, subject, request) {
+			let message = this.createMessage(receiver, subject, request);
+			this.process(receiver, message);
+		},
+		createMessage(receiver, subject, request) {
+			let message = this.sys.extend();
+			message.action = subject;
+			message.request = request;
+			message.status = 0;
+			return message;
+		},
+		process: function(receiver, message) {
+			let xhr = this.createRequest(receiver, message);
+
+			let method = this.getMethod(receiver, message);
+			let url = this.getUrl(receiver, message);
+			xhr.open(method, url);
+			
+			this.prepare(xhr);
+			
+			let content = this.getContent(receiver, message);
+			xhr.send(content);
+		},
+		createRequest: function(receiver, message) {
+			let xhr = new XMLHttpRequest();
+			xhr.receiver = receiver;
+			xhr.message = message;
+			xhr.onreadystatechange = () => this.monitor(xhr);
+			return xhr;
+		},
+		prepare: function(xhr) {
+			let header = this.getHeaders(xhr.receiver, xhr.message);
+			for (let name in header) {
+				let value = header[name];
+				value && xhr.setRequestHeader(name, value);
+			}
+		},
+		monitor: function(xhr) {
+			switch (xhr.readyState) {
+				case 0: // UNSENT Client has been created. open() not called yet.
+				case 1: // OPENED open() has been called.
+				case 2: // HEADERS_RECEIVED send() has been called, and headers and status are available.
+				case 3:	// LOADING Downloading; responseText holds partial data.
+					break;
+				case 4: // DONE The operation is complete.
+					xhr.message.status = xhr.status;
+					xhr.message.content = xhr.responseText;
+					if (typeof xhr.receiver == "function") {
+						xhr.receiver(xhr.message);
+					} else if (xhr.receiver) {
+						xhr.receiver.receive(xhr.message);
+					}
+			}
+		},
+		getMethod: function(on, message) {
+			return message.request.method || this.method;
+		},
+		getUrl: function(on, message) {
+			let requestUrl = message.request.url || "";
+			return this.url + requestUrl;
+		},
+		getHeaders: function(on, message) {
+			return null;
+//			{
+//				"Session-Id": this.session.id
+//			}
+		},
+		getContent: function(on, message) {
+			let content = message.request.content;
+			if (typeof content != "string") content = JSON.stringify(content);
+			return content;
+		}
+	}
+}
