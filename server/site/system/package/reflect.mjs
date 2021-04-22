63adf739622ff96a9ee169ffe5f49aa8717b7b90
jdhal <jdhaley@gmail.com>
2021-04-22 08:02:03 -0400

    work on reflect package

--- server/site/system/package/reflect.mjs
+++ server/site/system/package/reflect.mjs
@@ -22,5 +22,33 @@ export default {
 		moduleType: "",
 		type$uses: "Array",
 		type$packages: "Parcel"
+	},
+	Modules: {
+		type$: "Instance",
+		use: {
+			type$Module: "Module"
+		},
+		compile: function(source) {
+			let module = this.sys.extend(this.use.Module, module);
+			let pkgs = this.sys.extend();
+			//Need to define the module packages prior to compiling packages to support in-module package deps.
+			this.sys.packages[module.id] = pkgs;
+			for (let name in module.packages) {
+				pkgs[name] = this.compilePackage(name);
+			}
+			this.sys.define(module, "packages", pkgs);
+			this.sys.define(sys.modules, module.id, module);
+			Object.freeze(module);
+			console.info("Loaded", module);
+			return module;
+		},
+		compilePackage: function(module, name) {
+			let pkg = module.packages[name];
+			let ctxName = module.id + "/" + name;
+			console.debug(`Compiling "${ctxName}"...`);
+			pkg = this.sys.compile(pkg, ctxName);
+			console.debug(`Compiled "${ctxName}".`);
+			return pkg;
+		}
 	}
 }
