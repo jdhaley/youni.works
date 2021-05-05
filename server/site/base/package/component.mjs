45bb80e83811396e8d61833a17ad63f685c29015
jdhal <jdhaley@gmail.com>
2021-05-05 09:13:44 -0400

    Component/Instance refactoring...
    Added more behavior to Instance.  A component is a receiver object.

--- server/site/base/package/component.mjs
+++ server/site/base/package/component.mjs
@@ -1,44 +1,14 @@
 export default {
 	type$: "/system.youni.works/core",
-	A: {
-		type$: "",
-		x: "x"
-	},
-	B: {
-		type$: "",
-		y: "y"
-	},
-	Test: {
-		type$: ["Instance", "A", "B"]
-	},
 	Component: {
 		type$: "Instance",
-		get$interface: function() {
-			return this.sys[this.sys.symbols.interface];
+		start: function(conf) {
 		},
-		super: function(name, ...args) {
-			const thisValue = this[name];
-			for (let proto = Object.getPrototypeOf(this); proto; proto = Object.getPrototypeOf(proto)) {
-				let protoValue = proto[name];
-				if (protoValue !== thisValue) {
-					if (typeof protoValue == "function") return protoValue.apply(this, args);
-					break;
-				}
-			}
-			throw new Error(`super "${name}" is not a method.`);
+		receive: function(msg) {
+			let action = this.actions[typeof msg == "string" ? msg : msg.subject];
+			action && action.call(this.actions, this, msg);			
 		},
-		implement: function(decls) {
-			return this.sys.implement(this, decls);
-		},
-		define: function(name, value, facetName) {
-			return this.sys.define(this, name, value, facetName);
-		},
-		instanceOf: function(object) {
-			return object && typeof object == "object" && Object.prototype.isPrototypeOf.call(object, this);
-		},
-		toString: function() {
-			//TODO test using Symbol.toPrimitive instead.
-			return "";
+		extend$actions: {	
 		}
 	}
 }
--- server/site/system/package/core.mjs
+++ server/site/system/package/core.mjs
@@ -20,6 +20,30 @@ export default {
 		type$: "Object",
 		get$sys: function() {
 			return this[Symbol.sys]; //Symbol.sys is defined through bootstrapping.
-		}
+		},
+		get$interface: function() {
+			return this.sys[this.sys.symbols.interface];
+		},
+		super: function(name, ...args) {
+			const thisValue = this[name];
+			for (let proto = Object.getPrototypeOf(this); proto; proto = Object.getPrototypeOf(proto)) {
+				let protoValue = proto[name];
+				if (protoValue !== thisValue) {
+					if (typeof protoValue == "function") return protoValue.apply(this, args);
+					break;
+				}
+			}
+			throw new Error(`super "${name}" is not a method.`);
+		},
+		toString: Object.prototype.toString,
+		valueOf: Object.prototype.valueOf,
+		// Not sure if wanted or needed...
+		// implement: function(decls) {
+		// 	return this.sys.implement(this, decls);
+		// },
+		// define: function(name, value, facetName) {
+		// 	return this.sys.define(this, name, value, facetName);
+		// },
+
 	}
 }
