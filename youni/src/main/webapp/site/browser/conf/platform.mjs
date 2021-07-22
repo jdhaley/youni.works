f5165b9b8155ddb158a2ad12ff50ea18d5b9e9d1
John Haley <jdhaley@gmail.com>
2021-07-22 13:03:29 -0400

    youni.works resurrection: remove old /youni folder

--- youni/src/main/webapp/site/browser/conf/platform.mjs
+++
@@ -1,9 +0,0 @@
-import devices	from "./devices.mjs";
-import range	from "./range.mjs";
-import node		from "./node.mjs";
-
-export default {
-	devices: devices,
-	range: range,
-	node: node
-}
--- youni/src/main/webapp/site/diagram/index.mjs
+++
@@ -1,5 +0,0 @@
-import system 	from "../system/main.mjs";
-import main		from "./main.mjs";
-
-const sys = system();
-main(sys);
