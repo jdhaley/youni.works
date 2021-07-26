56cc2009514c01e042d531135d3ad1711216ff48
John Haley <jdhaley@gmail.com>
2021-07-26 14:45:38 -0400

    server/compiler for new compiler.

---
+++ server/compiler/index.mjs
@@ -0,0 +1,20 @@
+import fs		from "fs";
+import fsp		from "fs/promises";
+import https	from "https";
+import express 	from "express";
+import conf		from "./conf.mjs";
+
+main(conf);
+
+function main(conf) {
+	let module = conf.compiler;
+	let compiler = module.create({
+		type$: "/compiler/ModuleCompiler",
+		fs: fsp
+	});
+	compiler.load("../../source");
+
+	const httpsServer = https.createServer();
+	// let info = `Service listening on HTTPS port "${conf.port}"`;
+	// httpsServer.listen(conf.port, () => console.info(info));
+};
