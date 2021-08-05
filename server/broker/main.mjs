1668179b1753499ce89af1d09bdca9f9b59ad3a5
John Haley <jdhaley@gmail.com>
2021-08-05 08:32:27 -0400

    pub/sub/broker servers created

---
+++ server/broker/main.mjs
@@ -0,0 +1,19 @@
+import fs		from "fs";
+import https	from "https";
+import conf		from "./conf.mjs";
+
+main(conf);
+
+function main(conf) {
+	let module = conf.compiler;
+	let compiler = module.create({
+		type$: "/compiler/ModuleCompiler",
+		context: "../..",
+		fs: fs
+	});
+	compiler.load("source");
+
+	const httpsServer = https.createServer();
+	let info = `Compiler listening on HTTPS port "${conf.port}"`;
+	httpsServer.listen(conf.port, () => console.info(info));
+};
---
+++ server/subscriber/conf.mjs
@@ -0,0 +1,6 @@
+import compiler	from "../../target/compiler.youni.works-1.0.mjs";
+
+export default {
+	port: 8888,
+	compiler: compiler
+}
