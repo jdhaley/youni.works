5b74644f7f314f512a31ab4835175f5de4715242
John Haley <jdhaley@gmail.com>
2021-08-13 04:02:07 -0400

    websocket (test) using socket.io

--- server/workbench/main.mjs
+++ server/workbench/main.mjs
@@ -1,4 +1,5 @@
 import fs		from "fs";
+import { Server } from "socket.io";
 import https	from "https";
 import express 	from "express";
 
@@ -14,4 +15,16 @@ export default function main(conf) {
 	const httpsServer = https.createServer(credentials, service);
 	let info = `Service listening on HTTPS port "${conf.port}"`;
 	httpsServer.listen(conf.port, () => console.info(info));
+
+	const io = new Server(httpsServer, {
+		// ...
+	});
+	  
+	io.on('connection', socket => {
+		console.log("New connection");
+
+		socket.on('receive', data => {
+			console.log("Receive:", data);
+		});
+	});
 }
