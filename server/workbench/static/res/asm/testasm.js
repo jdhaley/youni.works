7db883455d6832d5c226106840f1046d95c5e0b2
John Haley <jdhaley@gmail.com>
2021-10-15 04:36:02 -0400

    updated testasm - chris' program.

--- server/workbench/static/res/asm/testasm.js
+++ server/workbench/static/res/asm/testasm.js
@@ -2,14 +2,13 @@ import assemble from "./asm.mjs";
 import exec from "./exec.mjs";
 let source = `
 Code
-set r0 total   ; total (start as seed)
-get r0 r0;
+set r0 27   ; seed
 set r1 4    ; increment
 set r2 2    ; fraction
-; r3 =scratch for conditions
-set r4 0;
+set r3 0    ; scratch reg. for conditions
+set r4 0;   ; loop count
 loop:
-    add r4 1; //loop count.
+    add r4 1;
     add r0 r1;
     set r3 r0;
     mod r3 2; 0 = even, 1 = odd;
@@ -20,13 +19,6 @@ loop:
     jnz r3 loop
     hlt;
 Data
-    total: 27
-            -32
-    limit: -1000000000
-    var:  7 60 61 62 63  ; comment test.
-    arr: *5
-    string: "Hello, world!!"
-    next: 13
 `
 let asm = assemble(source);
 document.body.innerHTML = asm.target;
