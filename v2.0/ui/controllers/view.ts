961b15e4bee7ef8fcfd171569cdec143d193a5ea
jdh <jdhaley@gmail.com>
2022-06-15 14:12:01 -0400

    text/html copiied onto clipboard.

--- v2.0/ui/controllers/view.ts
+++ v2.0/ui/controllers/view.ts
@@ -1,6 +1,6 @@
-import {extend} from "../../util.js";
+import {CHAR, extend} from "../../util.js";
 import {typeOf} from "../../model.js";
-import {ViewType} from "../views/view.js";
+import {ViewType, View} from "../views/view.js";
 import {UserEvent} from "../ui.js";
 import {copyRange} from "../article.js";
 
@@ -11,8 +11,9 @@ export default extend(null, {
 		let copy = copyRange(range, this);
 		let content = this.toModel(copy);
 		event.clipboardData.setData("application/json", JSON.stringify(content));
-		//TODO transform content into simple HTML markup.
-		//event.clipboardData.setData("text/html", copy.outerHTML);
+		let html = htmlify(copy);
+		console.log(html)
+		event.clipboardData.setData("text/html", html.outerHTML);
 		let data = "";
 		if (typeOf(content) == "text") {
 			data = "" + content;
@@ -48,3 +49,38 @@ export default extend(null, {
 		console.log(range.commonAncestorContainer.nodeName);
 	}
 });
+
+function htmlify(view: HTMLElement): HTMLElement {
+	let html: HTMLElement;
+	switch (view.tagName.toLowerCase()) {
+		case "ui-record":
+			html = view.ownerDocument.createElement("div");
+			html.innerHTML = "<strong style='color: gray'>" + view.dataset.type + ": </strong>";
+			html.className = "record";
+			for (let child of view.children) {
+				let prop = view.ownerDocument.createElement("div");
+				let caption = "<em style='color: gray'>" + (child as HTMLElement).dataset.name + ": </em>";
+				if (prop.tagName == "ui-text") {
+					prop.innerHTML = caption + (child.textContent == CHAR.ZWSP ? "" : child.innerHTML);
+				} else {
+					prop.innerHTML = caption + htmlify(child as HTMLElement).innerHTML;
+				}
+				html.append(prop);
+			}
+			return html;
+		case "ui-list":
+			html = view.ownerDocument.createElement("ol");
+			html.className = "list";
+			for (let child of view.children) {
+				let li = view.ownerDocument.createElement("li");
+				li.append(htmlify(child as HTMLElement));
+				html.append(li)
+			}
+			return html;		
+		default:
+			html = view.ownerDocument.createElement("span");
+			html.className = "text";
+			html.innerHTML = view.innerHTML;
+			return html;
+	}
+}
