4207e45f54b89c7ba90f521839dbfeeb463ac9a3
jdh <jdhaley@gmail.com>
2022-07-23 22:14:16 -0400

    dbl click hacked for tasks list.

--- v2/ui/controllers/list.ts
+++ v2/ui/controllers/list.ts
@@ -1,6 +1,19 @@
 import {extend} from "../../base/util.js";
+import { Editor } from "../article.js";
+import { Display, getHeader } from "../display.js";
+import { UserEvent } from "../ui.js";
 import view from "./view.js";
 export default extend(view, {
+	dblclick(this: Editor, event: UserEvent) {
+		event.subject = "";
+		let view = event.on as Display;
+		if (getHeader(event.on, event.target as Node)) {
+			if(view.type$.propertyName=="tasks") {
+				let content = view.v_content;
+				content.hidden = content.hidden ? false : true;
+			}
+		}
+	}
 	// copy(this: Editor, event: UserEvent) {
 	// 	event.subject = "";
 	// 	let range = this.owner.selectionRange;
