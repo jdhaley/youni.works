e0c4358362d297ddb7d90a14a1c40683aac1703f
jdh <jdhaley@gmail.com>
2022-08-09 13:51:55 -0400

    frame - some work on selection change (commented out)

--- v2/ui/controllers/frame.ts
+++ v2/ui/controllers/frame.ts
@@ -1,26 +1,47 @@
 import {Actions} from "../../base/controller.js";
+import { rangeIterator } from "../editor/util.js";
 import {Frame, UserEvent} from "../ui.js";
 
 let TRACK: UserEvent = null;
-let SELECTION: UserEvent = null;
+
+export function selection(range: Range) {
+    let doc = range.commonAncestorContainer.ownerDocument;
+    for (let ele of doc.getElementsByClassName("selected")) {
+        ele.classList.remove("selected");
+    }
+    let it = rangeIterator(range);
+	for (let node = it.nextNode(); node; node = it.nextNode()) {
+		if (node.nodeType == Node.TEXT_NODE) {
+            if (range.startContainer == node || range.endContainer == node) {
+                node.parentElement.classList.remove("selected");
+            }
+		} else if (node instanceof Element && node.classList.contains("view")) {
+            node.parentElement.classList.add("selected");
+        }
+	}
+}
 
 export default {
-    // selectionchange(event: UserEvent) {
-    //     //selectionchange comes from the Document, not the Window.
-    //     event.range = ownerOf(event.target as Node).selectionRange;
-    //     event.source = viewOf(event.range.commonAncestorContainer);
-    //     event.subject = "select";
-    //     if (SELECTION) {
-    //         if (SELECTION.source != event.source) {
-    //             SELECTION.subject = "unselect";
-    //             SELECTION.range = event.range;
-    //             sense(SELECTION);    
-    //         }
-    //         if (SELECTION.source == event.source) event.subject = "selecting";
-    //     }
-    //     SELECTION = event;
-    //     sense(event);
-    // },
+    selectionchange(event: UserEvent) {
+        let owner = ownerOf(event.target as Node);
+        let range = owner.selectionRange;
+    //    selection(range);
+
+        // //selectionchange comes from the Document, not the Window.
+        // event.range = ownerOf(event.target as Node).selectionRange;
+        // event.source = viewOf(event.range.commonAncestorContainer);
+        // event.subject = "select";
+        // if (SELECTION) {
+        //     if (SELECTION.source != event.source) {
+        //         SELECTION.subject = "unselect";
+        //         SELECTION.range = event.range;
+        //         sense(SELECTION);    
+        //     }
+        //     if (SELECTION.source == event.source) event.subject = "selecting";
+        // }
+        // SELECTION = event;
+        // sense(event);
+    },
     input: sense,
     cut: rangeEvent,
     copy: rangeEvent,
